a5adddea9692a107444bf3b4af31755695b700f8
RyougiNevermore <ryougi.nevermore@hotmail.com>
2018-10-09 05:02:02 +0800

    update code wave
    add crud code wave

--- codewave/table_getone.go
+++ codewave/table_getone.go
@@ -1,7 +1,115 @@
 package codewave
 
-import "github.com/pharosnet/dalg/def"
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/pharosnet/dalg/def"
+	"os"
+	"strings"
+)
 
-func waveTableGetOne(w Writer, table *def.Interface)  {
-
+func waveTableGetOne(w Writer, table *def.Interface) {
+	ql, qlErr := buildGetOneSql(table)
+	if qlErr != nil {
+		panic(qlErr)
+		os.Exit(1)
+	}
+	w.WriteString(fmt.Sprintf("const %sGetOneSQL = `%s` \n", toCamel(table.MapName, false), ql))
+	pkArgs := ""
+	for _, pk := range table.Pks {
+		pkArgs = pkArgs + fmt.Sprintf(", %s %s", toCamel(pk.MapName, false), pk.MapType)
+	}
+	w.WriteString(fmt.Sprintf("func %sGetOne(ctx context.Context%s) (row *%s, err error) { \n",
+		toCamel(table.MapName, true), pkArgs, toCamel(table.MapName, true)))
+	w.WriteString(`	queryFn := func(ctx context.Context, rows *sql.Rows, rowErr error) (err error) {`)
+	w.WriteString("\n")
+	w.WriteString(`		if rowErr != nil {`)
+	w.WriteString("\n")
+	w.WriteString(`			err = rowErr`)
+	w.WriteString("\n")
+	w.WriteString(`			return`)
+	w.WriteString("\n")
+	w.WriteString(`		}`)
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`		%s, scanErr := %sScan(rows)`, toCamel(table.MapName, false), toCamel(table.MapName, false)))
+	w.WriteString("\n")
+	w.WriteString(`		if scanErr != nil {`)
+	w.WriteString("\n")
+	w.WriteString(`			err = scanErr`)
+	w.WriteString("\n")
+	w.WriteString(`			return`)
+	w.WriteString("\n")
+	w.WriteString(`		}`)
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`		row = %s`, toCamel(table.MapName, false)))
+	w.WriteString("\n")
+	w.WriteString(`		return`)
+	w.WriteString("\n")
+	w.WriteString(`	}`)
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`	if err = dalc.Query(ctx, %sGetOneSQL, queryFn, id); err != nil {`, toCamel(table.MapName, false)))
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`		err = fmt.Errorf("%s: get one failed, %s", err)`, toCamel(table.MapName, true), "%v"))
+	w.WriteString("\n")
+	w.WriteString(`	}`)
+	w.WriteString("\n")
+	w.WriteString(`	return`)
+	w.WriteString("\n")
+	w.WriteString(`}`)
+	w.WriteString("\n")
+	w.WriteString("\n")
 }
+
+
+func buildGetOneSql(table *def.Interface) (ql string, err error) {
+	switch table.Dialect {
+	case "postgres":
+		ql, err = buildPostgresGetOneSql(table)
+	case "mysql":
+		ql, err = buildMysqlGetOneSql(table)
+	case "oracle":
+		ql, err = buildOracleGetOneSql(table)
+	default:
+		err = errors.New("build sql failed, unsupported dialect")
+	}
+	return
+}
+
+func buildPostgresGetOneSql(table *def.Interface) (ql string, err error) {
+	bb := bytes.NewBuffer([]byte{})
+	bb.WriteString(`SELECT `)
+	for i, col := range table.Columns {
+		if i > 0 {
+			bb.WriteString(", ")
+		}
+		bb.WriteString(fmt.Sprintf(`"%s"`, strings.TrimSpace(col.Name)))
+	}
+	if table.Schema != "" {
+		bb.WriteString(fmt.Sprintf(` FROM "%s"."%s" WHERE `, table.Schema,  table.Name))
+	} else {
+		bb.WriteString(fmt.Sprintf(` FROM "%s" WHERE `, table.Name))
+	}
+	for pi, pk := range table.Pks {
+		if pi > 0 {
+			bb.WriteString(` AND `)
+		}
+		bb.WriteString(fmt.Sprintf(`"%s" = $%d`, strings.TrimSpace(pk.Name), pi + 1))
+	}
+	ql = strings.TrimSpace(strings.ToUpper(bb.String()))
+	return
+}
+
+func buildMysqlGetOneSql(table *def.Interface) (ql string, err error) {
+
+	return
+}
+
+
+func buildOracleGetOneSql(table *def.Interface) (ql string, err error) {
+
+	return
+}
+
+
+
--- codewave/table_insert.go
+++ codewave/table_insert.go
@@ -1,7 +1,107 @@
 package codewave
 
-import "github.com/pharosnet/dalg/def"
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/pharosnet/dalg/def"
+	"os"
+	"strings"
+)
 
 func waveTableInsert(w Writer, table *def.Interface)  {
+	ql, qlErr := buildInsertSql(table)
+	if qlErr != nil {
+		panic(qlErr)
+		os.Exit(1)
+	}
+	w.WriteString(fmt.Sprintf("const %sInsertSQL = `%s` \n", toCamel(table.MapName, false), ql))
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`func %sInsert(ctx context.Context, rows ...*%s) (affected int64, err error) {`, toCamel(table.MapName, true),toCamel(table.MapName, true)))
+	w.WriteString("\n")
+	w.WriteString(`	fn := func(ctx context.Context, stmt *sql.Stmt, arg interface{}) (result sql.Result, err error) {`)
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`		row, ok := arg.(*%s)`, toCamel(table.MapName, true)))
+	w.WriteString("\n")
+	w.WriteString(`		if !ok {`)
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`			err = errors.New("%s: insert failed, invalid type")`, toCamel(table.MapName, true)))
+	w.WriteString("\n")
+	w.WriteString(`			return`)
+	w.WriteString("\n")
+	w.WriteString(`		}`)
+	w.WriteString("\n")
+	w.WriteString(`		result, err = stmt.Exec(`)
+	for _, col := range table.Columns {
+		w.WriteString(fmt.Sprintf(`			&row.%s,`, toCamel(col.MapName, true)))
+		w.WriteString("\n")
+	}
+	w.WriteString(`		)`)
+	w.WriteString("\n")
+	w.WriteString(`		return`)
+	w.WriteString("\n")
+	w.WriteString(`	}`)
+	w.WriteString("\n")
+	w.WriteString(fmt.Sprintf(`	affected, err = dalc.Execute(ctx, %sInsertSQL, fn, %sArrayMapToInterfacs(rows)...)`, toCamel(table.MapName, false), toCamel(table.MapName, false)))
+	w.WriteString("\n")
+	w.WriteString(`	return`)
+	w.WriteString("\n")
+	w.WriteString(`}`)
+	w.WriteString("\n")
+	w.WriteString("\n")
+}
 
+
+func buildInsertSql(table *def.Interface) (ql string, err error) {
+	switch table.Dialect {
+	case "postgres":
+		ql, err = buildPostgresInsertSql(table)
+	case "mysql":
+		ql, err = buildMysqlInsertSql(table)
+	case "oracle":
+		ql, err = buildOracleInsertSql(table)
+	default:
+		err = errors.New("build sql failed, unsupported dialect")
+	}
+	return
+}
+
+func buildPostgresInsertSql(table *def.Interface) (ql string, err error) {
+	bb := bytes.NewBuffer([]byte{})
+	if table.Schema != "" {
+		bb.WriteString(fmt.Sprintf(`INSERT INTO "%s"."%s" (`, table.Schema, table.Name))
+	} else {
+		bb.WriteString(fmt.Sprintf(`INSERT INTO "%s" (`, table.Name))
+	}
+	for i, col := range table.Columns {
+		if i == 0 {
+			bb.WriteString(`"` + strings.TrimSpace(col.Name) + `"`)
+		} else {
+			bb.WriteString(`, "` + strings.TrimSpace(col.Name) + `"`)
+		}
+	}
+	bb.WriteString(`) VALUES (`)
+	colLen := len(table.Columns)
+	for i := 1 ; i <= colLen ; i ++ {
+		if i == 1 {
+			bb.WriteString(fmt.Sprintf("$%d", i))
+		} else {
+			bb.WriteString(fmt.Sprintf(", $%d", i))
+		}
+	}
+	bb.WriteString(`)`)
+	ql = strings.TrimSpace(strings.ToUpper(bb.String()))
+	return
+}
+
+
+func buildMysqlInsertSql(table *def.Interface) (ql string, err error) {
+
+	return
+}
+
+
+func buildOracleInsertSql(table *def.Interface) (ql string, err error) {
+
+	return
 }
